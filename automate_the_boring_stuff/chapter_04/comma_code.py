8bdd9e60cee939b64d7472e5df886553c5d2198c
Marzi Dezfoulianrad <marzi.dezfoulianrad@gmail.com>
2025-07-07 22:08:30 +0330

    Update comma_code.py

--- automate_the_boring_stuff/chapter_04/comma_code.py
+++ automate_the_boring_stuff/chapter_04/comma_code.py
@@ -1,1 +1,17 @@
+def main (hashtags):
+    for index in range(len(hashtags)):
 
+        if index == len(hashtags) - 1:
+            print(f'{hashtags[index]}', end ='')
+
+        elif index == len(hashtags) - 2:
+            print(f'{hashtags[index]} ,and ', end= ' ')
+
+        else:
+            print(f'{hashtags[index]}, ', end= '')
+        
+    print()
+
+hashtags = input("What is in you mind? (split with a ,) ").split(',')
+
+main(hashtags)
